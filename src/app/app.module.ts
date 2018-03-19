2e1b035a29dd9ad5749a2e3ca29dcf740c291e05
Juan Cuadra <jcb8793@gmail.com>
2018-03-19 15:08:16 -0600

    Add iOS and Native File Packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { File } from '@ionic-native/file';
+import { FileTransfer } from '@ionic-native/file-transfer';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -23,6 +26,8 @@ import { HomePage } from '../pages/home/home';
   ],
   providers: [
     StatusBar,
+    File,
+    FileTransfer,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
